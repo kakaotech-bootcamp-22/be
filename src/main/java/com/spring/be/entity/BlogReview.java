7610fcad2cb1dbaa9438095feb32750d70b678aa
sall <keinpark1@naver.com>
2024-11-01 15:26:09 +0900

    feat : 데이터베이스 테이블 생성
    Common 테이블은 처음작성해봐서 저게맞는지 한번확인해주세요

---
+++ src/main/java/com/spring/be/entity/BlogReview.java
@@ -0,0 +1,33 @@
+package com.spring.be.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "blog_reviews")
+@Getter @Setter
+public class BlogReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long blogReviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id", nullable = false)
+    private Blog blog;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer likesCnt;
+
+    // Getters and Setters
+}
