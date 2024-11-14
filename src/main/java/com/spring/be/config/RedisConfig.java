d98dd7d0d3917d8b787da36017a9531826daa7cf
nuyeo <bluelayzxx@gmail.com>
2024-11-14 13:57:50 +0900

    refactor: RedisTemplate를 StringRedisTemplate으로 변경
    - string 직렬화를 위한 StringRedisSerializer 설정 제거

--- src/main/java/com/spring/be/config/RedisConfig.java
+++ src/main/java/com/spring/be/config/RedisConfig.java
@@ -3,23 +3,13 @@ package com.spring.be.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
-
-        return template;
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+       return new StringRedisTemplate(connectionFactory);
     }
 }
