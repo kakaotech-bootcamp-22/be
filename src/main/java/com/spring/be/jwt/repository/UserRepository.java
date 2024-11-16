57eefd5738e4650dc7af6b276eedad99d36c0ffe
dayaelee <dayae.dev@gmail.com>
2024-11-16 22:44:10 +0900

    KAN-72 refactor: 소셜 ID로 사용자 조회를 위한 리포지토리 쿼리 메서드 수정

--- src/main/java/com/spring/be/jwt/repository/UserRepository.java
+++ src/main/java/com/spring/be/jwt/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.spring.be.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findBySocialId(Long socialId);
 }
