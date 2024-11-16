8b763d8a3cbb6de0a2f7f21274cde5d28b6624b2
dayaelee <dayae.dev@gmail.com>
2024-11-16 22:44:25 +0900

    KAN-72 feat: 조건부 사용자 생성 및 카카오 액세스 토큰 업데이트 로직 구현

--- src/main/java/com/spring/be/jwt/service/UserService.java
+++ src/main/java/com/spring/be/jwt/service/UserService.java
@@ -15,8 +15,27 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public User saveUser(String platform, Long kakaoUserId, String nickname, String profileImage) {
-        User user = new User(platform, kakaoUserId, nickname, profileImage);
-        return userRepository.save(user);
+    // socialId로 사용자 존재 여부 확인 후, 없으면 새로 추가, 있으면 업데이트
+    public User saveUser(String platform, Long socialId, String nickname, String profileImage, String kakaoAccessToken) {
+        // 기존 사용자 찾기
+        User existingUser = userRepository.findBySocialId(socialId);
+
+        if (existingUser != null) {
+            // 기존 사용자 정보 업데이트
+            existingUser.setNickname(nickname);
+            existingUser.setUserImage(profileImage);
+            existingUser.setKakaoAccessToken(kakaoAccessToken);
+            return userRepository.save(existingUser); // 기존 사용자 업데이트
+        } else {
+            // 새로운 사용자 저장
+            User newUser = new User(platform, socialId, nickname, profileImage, kakaoAccessToken);
+            return userRepository.save(newUser); // 새로운 사용자 저장
+        }
     }
+
+    public User findBySocialId(Long socialId) {
+        return userRepository.findBySocialId(socialId);
+    }
+
+
 }
