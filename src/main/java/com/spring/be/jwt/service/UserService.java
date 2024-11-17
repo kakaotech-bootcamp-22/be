5bf0d61d34a74c88636c7e1314f4cc7b3c481c63
dayaelee <dayae.dev@gmail.com>
2024-11-18 04:31:05 +0900

    KAN-72 refactor: 엔티티 컬럼 타입 변경에 따른 UserService 수정

--- src/main/java/com/spring/be/jwt/service/UserService.java
+++ src/main/java/com/spring/be/jwt/service/UserService.java
@@ -5,6 +5,8 @@ import com.spring.be.jwt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+
 @Service
 public class UserService {
 
@@ -16,7 +18,7 @@ public class UserService {
     }
 
     // socialId로 사용자 존재 여부 확인 후, 없으면 새로 추가, 있으면 업데이트
-    public User saveUser(String platform, Long socialId, String nickname, String profileImage, String kakaoAccessToken) {
+    public User saveUser(String platform, BigInteger socialId, String nickname, String profileImage, String AccessToken) {
         // 기존 사용자 찾기
         User existingUser = userRepository.findBySocialId(socialId);
 
@@ -24,16 +26,16 @@ public class UserService {
             // 기존 사용자 정보 업데이트
             existingUser.setNickname(nickname);
             existingUser.setUserImage(profileImage);
-            existingUser.setKakaoAccessToken(kakaoAccessToken);
-            return userRepository.save(existingUser); // 기존 사용자 업데이트
+            existingUser.setAccessToken(AccessToken);
+            return userRepository.save(existingUser);
         } else {
             // 새로운 사용자 저장
-            User newUser = new User(platform, socialId, nickname, profileImage, kakaoAccessToken);
+            User newUser = new User(platform, socialId, nickname, profileImage, AccessToken);
             return userRepository.save(newUser); // 새로운 사용자 저장
         }
     }
 
-    public User findBySocialId(Long socialId) {
+    public User findBySocialId(BigInteger socialId) {
         return userRepository.findBySocialId(socialId);
     }
 
