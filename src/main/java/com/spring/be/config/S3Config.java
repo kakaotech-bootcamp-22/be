f1a50bec6f0924194b56301939532a7fb5a7d6ae
dayaelee <dayae.dev@gmail.com>
2024-11-21 02:33:53 +0900

    KAN-72 feat: AWS S3 연동을 위한 기본 설정 클래스 추가

---
+++ src/main/java/com/spring/be/config/S3Config.java
@@ -0,0 +1,26 @@
+package com.spring.be.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client(
+            @Value("${spring.cloud.aws.credentials.accessKey}") String accessKey,
+            @Value("${spring.cloud.aws.credentials.secretKey}") String secretKey,
+            @Value("${spring.cloud.aws.region.static}") String region
+    ) {
+        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+}
