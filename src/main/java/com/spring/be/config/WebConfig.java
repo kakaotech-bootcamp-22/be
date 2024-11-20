fc7d864fd484d2e92d43729b7ca637d211dbc5bb
nuyeo <bluelayzxx@gmail.com>
2024-11-20 21:17:59 +0900

    feat: CORS 허용 헤더 설정 추가

--- src/main/java/com/spring/be/config/WebConfig.java
+++ src/main/java/com/spring/be/config/WebConfig.java
@@ -8,10 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
+                .allowedOrigins("http://localhost:3000") // 허용할 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
+                .allowedHeaders("*") // 허용할 헤더
+                .allowCredentials(true); // 인증 정보 포함
     }
 }
 
