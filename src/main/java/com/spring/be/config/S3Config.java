37eb47ddf14020158d6875d262ecf7d5aa5a16e6
dayaelee <dayae.dev@gmail.com>
2024-11-25 07:28:19 +0900

    KAN-72 refactor: S3Config 프로퍼티 주입 방식으로 변경

--- src/main/java/com/spring/be/config/S3Config.java
+++ src/main/java/com/spring/be/config/S3Config.java
@@ -1,22 +1,32 @@
 package com.spring.be.config;
 
+
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
+
 @Configuration
 public class S3Config {
 
+    private final Environment environment;
+
+    public S3Config(Environment environment){
+        this.environment = environment;
+    }
+
     @Bean
-    public S3Client s3Client(
-            @Value("${spring.cloud.aws.credentials.accessKey}") String accessKey,
-            @Value("${spring.cloud.aws.credentials.secretKey}") String secretKey,
-            @Value("${spring.cloud.aws.region.static}") String region
-    ) {
+    public S3Client s3Client() {
+        String accessKey = environment.getProperty("spring.cloud.aws.credentials.accessKey");
+        String secretKey = environment.getProperty("spring.cloud.aws.credentials.secretKey");
+        String region = environment.getProperty("spring.cloud.aws.region.static");
+
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
