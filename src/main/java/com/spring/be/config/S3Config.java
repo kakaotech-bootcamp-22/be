0c6555be4b4f1542d9178cbb818f58f9492affc5
dayaelee <dayae.dev@gmail.com>
2024-11-28 00:09:12 +0900

    KAN-72 feat: AWS S3 클라이언트 설정 추가

--- src/main/java/com/spring/be/config/S3Config.java
+++ src/main/java/com/spring/be/config/S3Config.java
@@ -1,32 +1,29 @@
 package com.spring.be.config;
 
-
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
-
 @Configuration
 public class S3Config {
 
-    private final Environment environment;
+    private final AwsProperties awsProperties;
 
-    public S3Config(Environment environment){
-        this.environment = environment;
+    public S3Config(AwsProperties awsProperties) {
+        this.awsProperties = awsProperties;
     }
 
     @Bean
     public S3Client s3Client() {
-        String accessKey = environment.getProperty("spring.cloud.aws.credentials.accessKey");
-        String secretKey = environment.getProperty("spring.cloud.aws.credentials.secretKey");
-        String region = environment.getProperty("spring.cloud.aws.region.static");
+        // AwsProperties에서 값 가져오기
+        String accessKey = awsProperties.getCredentials().getAccessKey();
+        String secretKey = awsProperties.getCredentials().getSecretKey();
+        String region = awsProperties.getRegion();
 
+        // S3Client 생성
         AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
                 .region(Region.of(region))
