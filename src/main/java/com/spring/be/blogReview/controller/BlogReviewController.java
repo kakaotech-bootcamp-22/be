955678606c994a448984bf6570ece4fa228a8bab
sall <keinpark1@naver.com>
2024-12-02 18:37:34 +0900

    KAN-112 feat: 페이지네이션 구현

--- src/main/java/com/spring/be/blogReview/controller/BlogReviewController.java
+++ src/main/java/com/spring/be/blogReview/controller/BlogReviewController.java
@@ -1,16 +1,17 @@
 package com.spring.be.blogReview.controller;
 
-import com.spring.be.blogReview.dto.BlogReviewResponseDto;
-import com.spring.be.blogReview.dto.ReviewLikeRequest;
-import com.spring.be.blogReview.dto.ReviewRequest;
+import com.spring.be.blogReview.dto.*;
 import com.spring.be.blogReview.service.BlogReviewService;
+import com.spring.be.entity.BlogReview;
 import com.spring.be.jwt.config.JwtUtils;
 import com.spring.be.util.CookieUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigInteger;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/review")
@@ -29,7 +30,7 @@ public class BlogReviewController {
 
     // 리뷰 등록
     @PostMapping
-    public ResponseEntity<String> createReview(
+    public ResponseEntity<ReviewResponseDto> createReview(
             @RequestBody ReviewRequest request,
             @RequestHeader("Cookie") String cookieHeader
     ) {
@@ -42,11 +43,11 @@ public class BlogReviewController {
             BigInteger socialId = new BigInteger(socialIdString);
 
             // 리뷰 등록
-            blogReviewService.saveReview(request, socialId);
+            Long blogReviewId = blogReviewService.saveReview(request, socialId);
 
-            return ResponseEntity.status(201).body("리뷰가 성공적으로 등록되었습니다.");
+            return ResponseEntity.status(201).body(new ReviewResponseDto(blogReviewId));
         } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(401).body(e.getMessage());
+            return ResponseEntity.status(401).body(null);
         }
     }
 
@@ -54,5 +55,33 @@ public class BlogReviewController {
     public ResponseEntity<String> likeReview(@RequestBody ReviewLikeRequest request) {
         blogReviewService.incrementLikes(request.getReviewId());
         return ResponseEntity.ok("좋아요 증가");
+    }
+
+    @GetMapping("/{blogId}/likes")
+    public Page<ReviewDto> getReviewsByLikesCnt(@PathVariable Long blogId,
+                                                @RequestParam(defaultValue = "0") int page,
+                                                @RequestParam(defaultValue = "5") int size) {
+        return blogReviewService.getReviewsByLikesCnt(blogId, page, size);
+    }
+
+    @GetMapping("/{blogId}/recent")
+    public Page<ReviewDto> getReviewsByCreatedAt(@PathVariable Long blogId,
+                                                  @RequestParam(defaultValue = "0") int page,
+                                                  @RequestParam(defaultValue = "5") int size) {
+        return blogReviewService.getReviewsByCreatedAt(blogId, page, size);
+    }
+
+    @GetMapping("/{blogId}/rating-desc")
+    public Page<ReviewDto> getReviewsByRatingDesc(@PathVariable Long blogId,
+                                                   @RequestParam(defaultValue = "0") int page,
+                                                   @RequestParam(defaultValue = "5") int size) {
+        return blogReviewService.getReviewsByRatingDesc(blogId, page, size);
+    }
+
+    @GetMapping("/{blogId}/rating-asc")
+    public Page<ReviewDto> getReviewsByRatingAsc(@PathVariable Long blogId,
+                                                  @RequestParam(defaultValue = "0") int page,
+                                                  @RequestParam(defaultValue = "5") int size) {
+        return blogReviewService.getReviewsByRatingAsc(blogId, page, size);
     }
 }
