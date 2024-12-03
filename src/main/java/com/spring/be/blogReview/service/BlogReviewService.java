d05cbdfb67d9c3686fe4a5469c5200d6d97e6b77
sall <keinpark1@naver.com>
2024-12-04 01:37:41 +0900

    KAN-112 chore: @Transactional 애너테이션 추가

--- src/main/java/com/spring/be/blogReview/service/BlogReviewService.java
+++ src/main/java/com/spring/be/blogReview/service/BlogReviewService.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
 import java.util.List;
@@ -76,6 +77,7 @@ public class BlogReviewService {
     }
 
     // 리뷰 등록 로직
+    @Transactional
     public Long saveReview(ReviewRequest request, BigInteger socialId) {
         Blog blog = blogRepository.findById(request.getBlogId())
                 .orElseThrow(() -> new IllegalArgumentException("Invalid blog ID"));
