02175a0a1b9744981b6fd443a1fcec824deb4315
nuyeo <bluelayzxx@gmail.com>
2024-12-04 13:33:24 +0900

    KAN-112 feat: 초기 데이터 삽입 쿼리에 생성일자 추가

--- src/main/java/com/spring/be/BlogDataInsert.java
+++ src/main/java/com/spring/be/BlogDataInsert.java
@@ -16,11 +16,12 @@ public class BlogDataInsert implements CommandLineRunner {
     @Transactional
     public void run(String... args) throws Exception {
         // 초기 데이터 삽입
-        String sql = "INSERT INTO blogs (blog_id, blog_url, review_count) " +
-                "VALUES (1, 'blog.naver.com/kakao_food_fighter', 0) " +
+        String sql = "INSERT INTO blogs (blog_id, blog_url, review_count, created_at) " +
+                "VALUES (1, 'blog.naver.com/kakao_food_fighter', 0, current_timestamp) " +
                 "ON CONFLICT (blog_id) DO NOTHING"; // 중복 방지
 
         entityManager.createNativeQuery(sql).executeUpdate();
         System.out.println("초기 데이터가 삽입되었습니다.");
     }
 }
+
