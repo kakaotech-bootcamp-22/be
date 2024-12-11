83d4b52bcb5aae29bc3c20eb36d84dea35ab7fd9
nuyeo <bluelayzxx@gmail.com>
2024-12-11 14:49:37 +0900

    KAN-138 refactor: AI 서버에서 오는 응답을 ReviewCheckResponse Dto로 매핑하여 처리하도록 수정

--- src/main/java/com/spring/be/reviewcheck/controller/ReviewCheckController.java
+++ src/main/java/com/spring/be/reviewcheck/controller/ReviewCheckController.java
@@ -2,6 +2,7 @@ package com.spring.be.reviewcheck.controller;
 
 import com.spring.be.entity.ReviewCheckResult;
 import com.spring.be.reviewcheck.dto.ReviewCheckRequest;
+import com.spring.be.reviewcheck.dto.ReviewCheckResponse;
 import com.spring.be.reviewcheck.service.ReviewCheckService;
 import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +32,7 @@ public class ReviewCheckController {
     // AI 서버 응답 처리: 분석 결과를 캐싱하고 데이터베이스에 저장
     @PostMapping("/response")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void receiveAIResponse(@RequestBody ReviewCheckResult result) {
+    public void receiveAIResponse(@RequestBody ReviewCheckResponse result) {
         try {
             // AI 서버의 응답을 캐시 및 데이터베이스에 저장
             reviewCheckService.cachedReviewCheckResult(result.getRequestId(), result);
