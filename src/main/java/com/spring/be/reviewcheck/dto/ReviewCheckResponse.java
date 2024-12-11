6b29811ec833bdec837a74f3a57b1f34092c20fe
nuyeo <bluelayzxx@gmail.com>
2024-12-11 14:45:02 +0900

    KAN-138 feat: ReviewCheckResponse Dto 정의

---
+++ src/main/java/com/spring/be/reviewcheck/dto/ReviewCheckResponse.java
@@ -0,0 +1,14 @@
+package com.spring.be.reviewcheck.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ReviewCheckResponse {
+    private String requestId;
+    private String blogId;
+    private String summaryTitle;
+    private String summaryText;
+    private Integer score;
+    private String evidence;
+}
