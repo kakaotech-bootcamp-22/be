45eda6ab458d61d5f52e237064fefe05332f1fdc
nuyeo <bluelayzxx@gmail.com>
2024-12-17 03:42:51 +0900

    chore: 프로덕션 도메인을 CORS 허용에 추가

--- src/main/java/com/spring/be/config/CorsConfig.java
+++ src/main/java/com/spring/be/config/CorsConfig.java
@@ -6,13 +6,18 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000"); // 허용할 도메인
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",    // 개발용 도메인
+                "https://gotitguy.com/"  // 프로덕션 도메인
+        ));
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         configuration.addAllowedHeader("*"); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 자격 증명 허용
