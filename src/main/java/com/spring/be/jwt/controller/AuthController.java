9dd9e49a18ee542252c69095191e9ff21d8fb227
dayaelee <dayae.dev@gmail.com>
2024-12-23 18:11:02 +0900

    KAN-145 feat: 컨트롤러에서 서비스 계층 반환 값 처리 로직 수정
    - 서비스 계층에서 DTO 반환 방식에 맞추어 컨트롤러에서 ResponseEntity 생성 로직 추가

--- src/main/java/com/spring/be/jwt/controller/AuthController.java
+++ src/main/java/com/spring/be/jwt/controller/AuthController.java
@@ -1,13 +1,15 @@
 package com.spring.be.jwt.controller;
 
 import com.spring.be.jwt.config.JwtUtils;
+import com.spring.be.jwt.dto.CommonResponseDto;
+import com.spring.be.jwt.dto.LogoutResponseDto;
+import com.spring.be.jwt.dto.UserLoginStatusDto;
 import com.spring.be.jwt.service.AuthService;
 import com.spring.be.user.dto.UserResponseDto;
 import com.spring.be.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import com.spring.be.util.CookieUtils;
 
 import java.util.Map;
 
@@ -32,25 +34,83 @@ public class AuthController {
     }
 
     @GetMapping("/status")
-    public ResponseEntity<?> checkLoginStatus(@RequestHeader("Cookie") String cookieHeader) {
-        String token = CookieUtils.extractTokenFromCookie(cookieHeader, "jwtToken");
-        return authService.checkLoginStatus(token);
+    public ResponseEntity<UserLoginStatusDto> checkLoginStatus(@CookieValue("jwtToken") String jwtToken) {
+        UserLoginStatusDto response = authService.checkLoginStatus(jwtToken);
+
+        if (!response.isLoggedIn()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/logout/kakao")
-    public ResponseEntity<?> kakaoLogout(@RequestHeader("Cookie") String cookieHeader) {
-        String token = CookieUtils.extractTokenFromCookie(cookieHeader, "jwtToken");
-        return authService.performKakaoLogout(token);
+    public ResponseEntity<CommonResponseDto> kakaoLogout(@CookieValue("jwtToken") String jwtToken) {
+        CommonResponseDto response = authService.performKakaoLogout(jwtToken);
+
+        if(!response.isSuccess()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+        }
+
+        // 로그아웃 성공시 사용자 세션 및 쿠키 해제
+        ResponseCookie jwtCookie = ResponseCookie.from("jwtToken", "")
+                .httpOnly(false)// 테스트용 - false
+                .secure(true) // 필요에 따라 설정
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(response);
     }
 
     @PostMapping("/logout/google")
-    public ResponseEntity<?> googleLogout(@RequestHeader("Cookie") String cookieHeader) {
-        String token = CookieUtils.extractTokenFromCookie(cookieHeader, "jwtToken");
-        return authService.performGoogleLogout(token);
+    public ResponseEntity<CommonResponseDto> googleLogout(@CookieValue("jwtToken") String jwtToken) {
+        CommonResponseDto response = authService.performGoogleLogout(jwtToken);
+
+        if (!response.isSuccess()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+
+        ResponseCookie jwtCookie = ResponseCookie.from("jwtToken", "")
+                .httpOnly(false)// 테스트용 - false
+                .secure(true) // 필요에 따라 설정
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(response);
     }
 
     @GetMapping("/delete")
-    public ResponseEntity<?> deleteUser(@CookieValue("jwtToken") String jwtToken) {
-        return authService.deleteUser(jwtToken);
+    public ResponseEntity<CommonResponseDto> deleteUser(@CookieValue("jwtToken") String jwtToken) {
+        CommonResponseDto response = authService.deleteUser(jwtToken);
+
+        if(!response.isSuccess()) {
+            if (response.getMessage().equals("Invalid user.")) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+            } else if (response.getMessage().equals("Unsupported platform.")) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            } else if (response.getMessage().equals("Logout failed.") || response.getMessage().equals("Failed to delete user.")) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+            }
+        }
+
+        ResponseCookie jwtCookie = ResponseCookie.from("jwtToken", "")
+                .httpOnly(false)// 테스트용 - false
+                .secure(true) // 필요에 따라 설정
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(response);
     }
 }
